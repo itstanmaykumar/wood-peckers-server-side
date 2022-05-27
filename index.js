3d043ada4787b4b45f0d02c8edea8ad7774b8939
Tanmay Kumar <itstanmaykumar@gmail.com>
2022-05-27 19:56:09 +0600

    added jwt

--- index.js
+++ index.js
@@ -11,9 +11,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//verying jwt token
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        //console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
 //Server Connections
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@wood-peckers.w1yek.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 async function run() {
     try {
@@ -23,6 +41,12 @@ async function run() {
         const database = client.db('woodpeckersDB');
         const productsCollection = database.collection('products');
 
+        //using jwt token to verify user
+        app.post('/signin', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+            res.send({ accessToken });
+        });
     }
     finally {
         // await client.close();
