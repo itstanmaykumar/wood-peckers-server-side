ffd4127c03cf23744f8d9f327b7a97b6ca43601a
Tanmay Kumar <itstanmaykumar@gmail.com>
2022-05-29 20:10:43 +0600

    admin fix

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ async function run() {
         app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
             const user = await usersCollection.findOne({ email: email });
-            const isAdmin = user.role === 'admin';
+            const isAdmin = user.role === true;
             res.send({ admin: isAdmin })
         })
 
