dfd8a4096ece619b9b242e05090d1e05a64cb1f8
Tanmay Kumar <itstanmaykumar@gmail.com>
2022-05-30 23:15:40 +0600

    fixing cors()

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: true,
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 //verying jwt token
